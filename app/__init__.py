4ea0b7acb3622407a0f597c01f961bc0fb6fffd5
Artem Ptashenko <aptashenko2019@gmail.com>
2024-07-25 19:09:00 +0300

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,11 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    app.config.from_object('app.configs.Config')
+    MYSQL_HOST = 'ovh6.mirohost.net'
+    MYSQL_USER = 'u_aptashenko'
+    MYSQL_PASSWORD = 'Aptashenko93'
+    MYSQL_DB = 'utprozorro_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
 
     db.init_app(app)
     migrate.init_app(app, db)
